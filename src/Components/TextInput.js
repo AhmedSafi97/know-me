5bb625f3cd4cdc0d17ad5c2e2734980d566ac2db
Ahmed Safi <ahmadhsafi1997@gmail.com>
2020-09-15 00:09:43 +0300

    created TextInput component (#29)

---
+++ src/Components/TextInput.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const TextInput = ({ children, placeholder, value, label, onChange }) => (
+  <div className="relative w-full max-w-sm m-auto">
+    {children}
+    <input
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      aria-label={label}
+      onChange={onChange}
+      className="block bg-gray-light text-gray-dark placeholder-gray-dark placeholder-opacity-100 rounded-full w-full py-4 px-12 m-auto"
+    />
+  </div>
+);
+
+TextInput.propTypes = {
+  children: propTypes.node.isRequired,
+  placeholder: propTypes.string.isRequired,
+  value: propTypes.string.isRequired,
+  label: propTypes.string.isRequired,
+  onChange: propTypes.func.isRequired,
+};
+
+export default TextInput;
