b7882e9c249e26be5f65f65fe02cddcbaebfb99e
Ahmed Safi <ahmadhsafi1997@gmail.com>
2020-10-01 08:13:59 +0300

    optmized loading bg image (#55)
    made an event listener to be triggered when image is fully loaded, spinner will be displayed while loading the image

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export { default as requestsListener } from './requestsListener';
 export { default as contactsListener } from './contactsListener';
 export { default as chatsListener } from './chatsListener';
+export { default as imageLoader } from './imageLoader';
